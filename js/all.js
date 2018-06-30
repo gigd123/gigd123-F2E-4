92064bfc45017b699f8ad3e195a4f34e702cbfb0
gigd123 <gigdleo@gmail.com>
2018-06-30 19:29:16 +0800

    Update 2018-06-30T11:29:12.610Z

---
+++ js/all.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var nF = function nF() {
+    console.log('a');
+};
+nF();
+'use strict';
+
+var nF2 = function nF2() {
+    console.log('a');
+};
+nF2();
+//# sourceMappingURL=all.js.map
--- source/js/all2.js
+++
@@ -1,4 +0,0 @@
-let nF2 = ()=>{
-    console.log('a');
-}
-nF2();
